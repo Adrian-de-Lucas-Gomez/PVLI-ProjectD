8ddc5f69b8f2b6af0afc75bd8587c5467acd2e83
auroraGJ <48923631+auroraGJ@users.noreply.github.com>
2019-10-16 19:46:54 +0200

    prueva fallida

--- phasertemplate-master/game.js
+++ phasertemplate-master/game.js
@@ -2,6 +2,7 @@ export default class Game extends Phaser.Scene {
   constructor() {
     super({ key: 'main' });
   }
+
   preload() {
     this.load.image('fondo', 'https://img-cdn.hipertextual.com/files/2019/06/hipertextual-estas-son-imagenes-que-se-juegan-premio-mejor-astrofotografo-ano-2019530966.jpg?strip=all&lossy=1&quality=57&resize=740%2C490&ssl=1');
     this.load.image('sprite', './descarga.jpg');
@@ -13,9 +14,9 @@ export default class Game extends Phaser.Scene {
     
     
     let image = this.add.image(400,300,'fondo');
-   // let sprite = new Phaser.GameObjects.Sprite(this, 400, 400, 'sprite');
-   let jugador = new Jugador(this,0,0)
-    this.add.existing(jugador);
+    let sprite = new Phaser.GameObjects.Sprite(this, 400, 400, 'sprite');
+    //let sprite = new Player(this,0,0)
+    this.add.existing(sprite);
     this.cameras.main.setViewport(0, 0, 800, 600);
   }
 
@@ -24,19 +25,4 @@ export default class Game extends Phaser.Scene {
   
   
   
-}
-class Jugador extends Phaser.GameObjects.sprite
-{
-  constructor(scene)
-  {
-    let x = 400;
-    let y = 400;
-    super(this, x,y,'sprite')
-  }
-
-  Preupdate(time,delta)
-  {
-    this.x =+ 10;
-
-  }
 }
