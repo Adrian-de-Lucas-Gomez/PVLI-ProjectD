d6e095ed75dc5572d01e3fa1f0d5c016a47d7a87
auroraGJ <48923631+auroraGJ@users.noreply.github.com>
2019-10-16 17:38:43 +0200

    Probando con sprites, prueba 1

--- phasertemplate-master/game.js
+++ phasertemplate-master/game.js
@@ -4,16 +4,16 @@ export default class Game extends Phaser.Scene {
   }
   preload() {
     this.load.image('fondo', 'https://img-cdn.hipertextual.com/files/2019/06/hipertextual-estas-son-imagenes-que-se-juegan-premio-mejor-astrofotografo-ano-2019530966.jpg?strip=all&lossy=1&quality=57&resize=740%2C490&ssl=1');
-   
+    this.load.image('sprite', './descarga.jpg');
    
   }
 
   create() {
-    //let player = scene.add.sprite(0, 0, 'fondo');
+    //let player = scene.add.sprite(0, 0, 'sprite');
     
     
     let image = this.add.image(400,300,'fondo');
-    let sprite = new Phaser.GameObjects.Sprite(this, 400, 400, 'sprit');
+    let sprite = new Phaser.GameObjects.Sprite(this, 400, 400, 'sprite');
     
     this.add.existing(sprite);
     this.cameras.main.setViewport(0, 0, 800, 600);
