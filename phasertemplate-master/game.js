90cb0dc4bed8ba977781c1ebc7197b1520d7b8d7
auroraGJ <48923631+auroraGJ@users.noreply.github.com>
2019-11-01 15:51:49 +0100

    los controles de las flechas ya funcionan

--- phasertemplate-master/game.js
+++ phasertemplate-master/game.js
@@ -45,16 +45,18 @@ export default class Game extends Phaser.Scene {
       this.a = this.input.keyboard.addKey('A');
       this.s = this.input.keyboard.addKey('S');
       this.d = this.input.keyboard.addKey('D');
+      this.cursor = this.input.keyboard.createCursorKeys();
+      console.log(this.cursor);
   }
 
   update(time, delta) {
-    if(this.w.isDown)
+    if(this.w.isDown || this.cursor.up.isDown)
     {
        //this.player.y-=5;
        this.player.body.setVelocityY(-100);
       
     }
-    else if(this.s.isDown)
+    else if(this.s.isDown || this.cursor.down.isDown)
     {
       this.player.body.setVelocityY(100);
     }
@@ -62,12 +64,12 @@ export default class Game extends Phaser.Scene {
     {
       this.player.body.setVelocityY(0);
     }
-     if(this.a.isDown)
+     if(this.a.isDown || this.cursor.left.isDown)
     {
        // this.player.x-=5;
        this.player.body.setVelocityX(-100)
     }
-    else if ( this.d.isDown)
+    else if ( this.d.isDown || this.cursor.right.isDown)
     {
       this.player.body.setVelocityX(100)
     }
